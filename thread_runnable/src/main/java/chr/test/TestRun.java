7afe13d7e551975ecbbcaa14cbe3e165aa12fa55
charlesalter <charlesalter@yeah.net>
2018-04-29 19:10:23 +0800

    lamda

--- thread_runnable/src/main/java/chr/test/TestRun.java
+++ thread_runnable/src/main/java/chr/test/TestRun.java
@@ -8,8 +8,18 @@ public class TestRun {
         MyRun myRun = new MyRun();
         Thread th1 = new Thread(myRun,"th1");
         Thread th2 = new Thread(myRun,"th2");
-
+        Thread th3 = new Thread(()-> {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName());
+            }
+        },"th3");
         th1.start();
         th2.start();
+        th3.start();
     }
 }
